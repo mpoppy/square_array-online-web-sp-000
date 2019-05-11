652943f6c5a8ce17fab69ac5be093b6b79358cc7
Marc Poppy <poppy.marc@gmail.com>
2019-05-11 03:31:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,3 +2,11 @@ def square_array(array)
   array.map! {|number| number ** 2}
     #how to update to return the new array
 end
+
+def square_array(array)
+  new_array = []
+  array.each do |number|
+    new_num = number ** 2
+    new_array << new_num
+  end
+end
